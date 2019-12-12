44ffed26fd990fbc4ff25d77e2b534b0671a21aa
mrCrendel <tggrmi@gmail.com>
2019-12-12 21:35:17 +0600

    ^ update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 CLASSIFIERS = [
-    "Development Status :: 1 - Beta",
+    "Development Status :: 3 - Alpha",
     "Environment :: Web Environment",
     "Framework :: Django",
     "Framework :: Django",
@@ -22,7 +22,7 @@ CLASSIFIERS = [
 ]
 
 setup(
-    name="django-seo-inline",
+    name="django_seo_inline",
     version='0.0.1',
     author="Ruslan Tolkun uulu",
     author_email="tggrmi@gmail.com",
@@ -32,10 +32,9 @@ setup(
     url="https://github.com/mrCrendel/django-seo-inline",
     download_url='https://github.com/mrCrendel/django-seo-inline/archive/0.0.1.tar.gz',
     keywords=['seo', 'inline', 'meta-data'],
-    packages=["django-seo-inline"],
+    packages=["django_seo_inline"],
     include_package_data=True,
     install_requires=open('requirements/requirements.txt').read().splitlines(),
-    tests_require=open('requirements/test.txt').read().splitlines(),
     classifiers=CLASSIFIERS,
     zip_safe=False,
 )
